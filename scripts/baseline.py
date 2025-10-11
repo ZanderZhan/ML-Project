7a5b23d5653f31941527274c4a6d422ff2c68c4e
Zander <zanderchim78@gmail.com>
2025-10-11 17:05:02 +0100

    add baseline handle script

---
+++ scripts/baseline.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from transformers import BertTokenizer
+
+
+# load data from preprocess dataset
+data = 'github-labels-top3-803k-train'
+data_set = pd.read_csv(f'../dataset/raw/{data}.csv')
+
+# tokenize `issue_data` data using `BertTokenizer`
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+encoded_data_train = tokenizer.batch_encode_plus(
+    data_set.issue_data.values,
+    add_special_tokens=True,
+    return_attention_mask=True,
+    padding='longest',
+    truncation=True,
+    return_tensors='pt'
+)
+
+input_ids_train = encoded_data_train['input_ids']
+attention_masks_train = encoded_data_train['attention_mask']
