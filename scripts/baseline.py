743c1ebb94e2d005915172fbcc3ab587ffd6a725
Zander <zanderchim78@gmail.com>
2025-10-13 11:13:00 +0100

    fix baseline data source

--- scripts/baseline.py
+++ scripts/baseline.py
@@ -4,12 +4,12 @@ from transformers import BertTokenizer
 
 # load data from preprocess dataset
 data = 'github-labels-top3-803k-train'
-data_set = pd.read_csv(f'../dataset/raw/{data}.csv')
+data_set = pd.read_csv(f'../dataset/preprocess/{data}.csv')
 
 # tokenize `issue_data` data using `BertTokenizer`
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
 encoded_data_train = tokenizer.batch_encode_plus(
-    data_set.issue_label.values,
+    data_set.issue_data.values,
     add_special_tokens=True,
     return_attention_mask=True,
     padding='longest',
